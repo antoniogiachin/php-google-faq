f9bb2b95209fcac60a539eff4c3c8d2c88dc7a56
antoniogiachin <giachinantonio@outlook.com>
2022-03-11 15:35:02 +0100

    stilizzazione completa

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 
-    
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="./style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -17,5 +18,77 @@
 </head>
 <body>
     
+
+    <!-- header -->
+    <header>
+
+        <!-- header top -->
+        <div class="h-top">
+
+            <!-- top-left -->
+            <div class="h-top-left">
+                <img src="img/google.jpeg" alt="g-logo">
+                <span>Privacy e termini</span>
+            </div>
+
+            <!-- top-right -->
+            <div class="h-top-right">
+                <i class="fas fa-th"></i>
+                <span>A</span>
+
+            </div>
+        </div>
+
+        <!-- header bottom -->
+        <div class="h-bottom">
+            <div class="col">
+                <span>Introduzione</span>
+            </div>
+            <div class="col">
+                <span>Norme sulla privacy</span>
+            </div>
+            <div class="col">
+                <span>Termini di servizio</span>
+            </div>
+            <div class="col">
+                <span>Tecnologie</span>
+            </div>
+            <div class="col">
+                <span>Domande frequenti</span>
+            </div>
+        </div>
+    </header>
+
+    <main>
+
+        <div class="container">
+
+            <div class="faq">
+                <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste, voluptates! Deserunt culpa fugiat ad? Alias, quas minima numquam laudantium nesciunt, eius et repellendus deleniti ad incidunt consequuntur ipsum ipsam similique?</h1>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem quam iure distinctio sint ipsa fuga repudiandae esse, corporis, similique iste ratione recusandae quod, adipisci nobis! Cum ipsum reiciendis veniam officiis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid odit voluptatum laudantium neque perspiciatis enim tempore earum, repellat voluptas inventore autem dolorem aut. Quasi earum alias, eos optio molestiae deleniti!
+                Possimus expedita dolores repudiandae veniam, nostrum laudantium excepturi sint praesentium. Consequatur officiis sit totam, nisi facere incidunt dolorem reiciendis voluptatem aut porro, dicta ad dolores quos quis quia nam hic?
+                Commodi perferendis voluptate totam earum doloribus, placeat facere odio deleniti distinctio vero ipsam itaque, magnam nostrum adipisci optio aperiam? Quidem voluptas cumque reiciendis quas nemo officia necessitatibus totam! Possimus, sequi?</p>
+            </div>
+
+            <div class="faq">
+                <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste, voluptates! Deserunt culpa fugiat ad? Alias, quas minima numquam laudantium nesciunt, eius et repellendus deleniti ad incidunt consequuntur ipsum ipsam similique?</h1>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem quam iure distinctio sint ipsa fuga repudiandae esse, corporis, similique iste ratione recusandae quod, adipisci nobis! Cum ipsum reiciendis veniam officiis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid odit voluptatum laudantium neque perspiciatis enim tempore earum, repellat voluptas inventore autem dolorem aut. Quasi earum alias, eos optio molestiae deleniti!
+                Possimus expedita dolores repudiandae veniam, nostrum laudantium excepturi sint praesentium. Consequatur officiis sit totam, nisi facere incidunt dolorem reiciendis voluptatem aut porro, dicta ad dolores quos quis quia nam hic?
+                Commodi perferendis voluptate totam earum doloribus, placeat facere odio deleniti distinctio vero ipsam itaque, magnam nostrum adipisci optio aperiam? Quidem voluptas cumque reiciendis quas nemo officia necessitatibus totam! Possimus, sequi?</p>
+            </div>
+
+            <div class="faq">
+                <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste, voluptates! Deserunt culpa fugiat ad? Alias, quas minima numquam laudantium nesciunt, eius et repellendus deleniti ad incidunt consequuntur ipsum ipsam similique?</h1>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem quam iure distinctio sint ipsa fuga repudiandae esse, corporis, similique iste ratione recusandae quod, adipisci nobis! Cum ipsum reiciendis veniam officiis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid odit voluptatum laudantium neque perspiciatis enim tempore earum, repellat voluptas inventore autem dolorem aut. Quasi earum alias, eos optio molestiae deleniti!
+                Possimus expedita dolores repudiandae veniam, nostrum laudantium excepturi sint praesentium. Consequatur officiis sit totam, nisi facere incidunt dolorem reiciendis voluptatem aut porro, dicta ad dolores quos quis quia nam hic?
+                Commodi perferendis voluptate totam earum doloribus, placeat facere odio deleniti distinctio vero ipsam itaque, magnam nostrum adipisci optio aperiam? Quidem voluptas cumque reiciendis quas nemo officia necessitatibus totam! Possimus, sequi?</p>
+            </div>
+
+        </div>
+
+    </main>
 </body>
 </html>
